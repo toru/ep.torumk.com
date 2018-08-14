34080bde57f3941c316805395936483fb681a4a5
Toru Maesaka <toru@torumk.com>
2018-08-13 20:07:01 -0700

    javascript: scope the nav variables

--- assets/aux.js
+++ assets/aux.js
@@ -1,13 +1,13 @@
 document.addEventListener('keyup', function(ev) {
   switch (ev.key) {
     case "h":
-      prev = document.querySelector('.adjacent-posts > .prev');
+      var prev = document.querySelector('.adjacent-posts > .prev');
       if (prev) {
         window.location.href = prev.href;
       }
       break;
     case "l":
-      next = document.querySelector('.adjacent-posts > .next');
+      var next = document.querySelector('.adjacent-posts > .next');
       if (next) {
         window.location.href = next.href;
       }
