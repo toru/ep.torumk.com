5b92e8619e24c1b752dbe86fc258130d459e2674
Toru Maesaka <toru@torumk.com>
2018-09-25 15:23:16 -0700

    javascript: encapsulate search code with ES6 class
    This means no IE support but that's fine at this point.

--- assets/aux.js
+++ assets/aux.js
@@ -15,35 +15,41 @@ document.addEventListener('keyup', function(ev) {
   }
 });
 
-function isLocalStorageAvailable() {
-  var s = "e";
-  try {
-    localStorage.setItem(s,s);
-    localStorage.removeItem(s);
-    return true;
-  } catch(e) {
-    return false;
+class SearchEngine {
+  constructor() {
+    this.idxPath = "/assets/fts.idx";
   }
-}
 
-function loadSearchIndex() {
-  if (self.fetch) {
-    fetch("/assets/fts.idx")
-      .then(function(res) {
-        if (res.ok) {
-          return res.json()
-        }
-        throw new Error('failed to download');
-      })
-      .then(function(parsed) {
-        // TODO: cache in localStorage
-        console.log(JSON.stringify(parsed));
-      })
-      .catch(function(err) {
-        console.log(err);
-      });
-  } else {
-    // TODO: fallback to XMLHttpRequest
-    return false;
+  static isLocalStorageAvailable() {
+    var s = "e";
+    try {
+      localStorage.setItem(s,s);
+      localStorage.removeItem(s);
+      return true;
+    } catch(e) {
+      return false;
+    }
+  }
+
+  loadSearchIndex() {
+    if (self.fetch) {
+      fetch("/assets/fts.idx")
+        .then(function(res) {
+          if (res.ok) {
+            return res.json()
+          }
+          throw new Error('failed to download');
+        })
+        .then(function(parsed) {
+          // TODO: cache in localStorage
+          console.log(JSON.stringify(parsed));
+        })
+        .catch(function(err) {
+          console.log(err);
+        });
+    } else {
+      // TODO: fallback to XMLHttpRequest
+      return false;
+    }
   }
 }
